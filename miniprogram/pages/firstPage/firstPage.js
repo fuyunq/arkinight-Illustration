ee927812e6f95660d10d7d5f29686f2a07cb7bd3
傅运强 <fuyunqiang@ziquntech.com>
2020-01-10 17:43:06 +0800

    Initial Commit

---
+++ miniprogram/pages/firstPage/firstPage.js
@@ -0,0 +1,88 @@
+// miniprogram/pages/firstPage/firstPage.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    array:[],
+    buttonList:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    const db = wx.cloud.database({
+      env:'test1-0woef'
+    });
+    db.collection('picture')
+      .where({
+        type: 1
+      })
+      .limit(10)
+      .get().then(res=>{
+        this.setData({
+          array:res.data
+        })
+        console.log(res);
+      });
+    db.collection('button_list')
+      .orderBy('weight', 'asc')
+      .limit(10)
+      .get().then(res=>{
+        this.setData({
+          buttonList: res.data
+        })
+      });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
