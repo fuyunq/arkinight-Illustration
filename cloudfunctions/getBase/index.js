9e73fe410f2c191a7d801a4add639fb6367e314b
傅运强 <fuyunqiang@ziquntech.com>
2020-01-19 16:29:33 +0800

    fix

--- cloudfunctions/getBase/index.js
+++ cloudfunctions/getBase/index.js
@@ -16,78 +16,42 @@ exports.main = async(event, context) => {
       name: cn
     }).limit(1)
     .get();
-
-  let skillList = [];
   let skills = res.data[0].skills;
-  skillList = await getSkillList(skills);
-  // for (let i = 0; i < skills.length; i++) {
-  //   let id = skills[i].skillId;
-  //   let skilldata = await db.collection('skill_info')
-  //     .where({
-  //       skillId: id
-  //     }).limit(1)
-  //     .get();
-
-  //   let skillInfo = [];
-  //   let levels = skilldata.data[0].levels;
-  //   for (let j = 0; j < levels.length; j++) {
-  //     let skillLevelInfo ={};
-  //     let level = levels[j];
-  //     skillLevelInfo.name = level.name;
-  //     if (level.skillType == 1) {
-  //       skillLevelInfo.skillType = '自动触发';
-  //     } else if (level.skillType == 2) {
-  //       skillLevelInfo.skillType = '手动触发';
-  //     } else {
-
-  //     }
-  //     let spData = level.spData;
-  //     skillLevelInfo.initSp = spData.initSp;
-  //     skillLevelInfo.maxChargeTime = spData.maxChargeTime;
-  //     skillLevelInfo.spCost = spData.spCost;
-  //     if (spData.spType == 1) {
-  //       skillLevelInfo.spType = '自动回复';
-  //     } else if (spData.spType == 2) {
-  //       skillLevelInfo.spType = '攻击回复';
-  //     } else if (spData.spType == 2) {
-  //       skillLevelInfo.spType = '受击回复';
-  //     }
-  //     let blackBoard = level.blackboard;
-  //     let map = new Map();
-  //     let description = level.description;
-  //     let list = description.match(/<@ba.vup>.*?<\/>/gi);
-  //     for (let f = 0; f < list.length; f++) {
-  //       let str = list[f];
-        
-  //       for (let k = 0; k < blackBoard.length; k++) {
-  //         if (str.indexOf(blackBoard[k].key) != 1) {
-            
-  //           if (str.indexOf('%') != -1) {
-  //             let num = blackBoard[k].value * 100 + '%';
-  //             description = description.replace(str, num);
-              
-  //             break;
-  //           } else {
-  //             let num = blackBoard[k].value * 1;
-  //             description = description.replace(str, num);
-  //             break;
-              
-  //           }
-  //         }
-          
-  //       }
-  //     }
-  //     skillLevelInfo.desc = description;
-  //     skillInfo.push(skillLevelInfo);
-  //   }
-  //   skillList.push(skillInfo);
-  // }
+  let skillList = await getSkillList(skills);
+    let baseInfoList = await getBaseInfoList(res.data[0]);
   return {
     skillList,
-    res
+    res,
+      baseInfoList
   };
 }
+async function getBaseInfoList(character){
+    let baseInfoList =[];
+    
+    let phases = character.phases;
+    for(let i =0;i<phases.length;i++){
+        let attributesKeyFrames = phases[i].attributesKeyFrames;
 
+        for (let j = 0; j < attributesKeyFrames.length;j++){
+            let attributesKeyFrame = attributesKeyFrames[j];
+            
+            let data = attributesKeyFrame.data;
+            let baseInfo = data;
+            let str ='';
+            if(i==0){
+                str='初始';
+            }else if(i==1){
+                str = '精一';
+            } else if (i == 2) {
+                str = '精二';
+            }
+            baseInfo.level = str + attributesKeyFrame.level+'级';
+            baseInfoList.push(baseInfo);
+        }
+        
+    }
+    return baseInfoList;
+}
 async function getSkillList(skills){
   let skillList = [];
   for (let i = 0; i < skills.length; i++) {
--- miniprogram/pages/peopleDetail/peopleDetail.js
+++ miniprogram/pages/peopleDetail/peopleDetail.js
@@ -5,7 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    baseInfo: {},
+    baseInfoList:[],
+    baseInfoState:false,
+    baseInfoButton:'展开',
     peopleImageState: false,
     peopleImageButton: '展开',
     peopleDetail: null,
@@ -35,76 +37,79 @@ Page({
     this.setData({
       icon: icon
     })
-    const db = wx.cloud.database({
-      env: 'test1-s9ptf'
-    });
-    db.collection('character')
-      .where({
-        name: cn
-      })
-      .limit(1)
-      .get().then(res => {
-        console.log(res);
-        this.setData({
-          desc: res.data[0].itemDesc,
-          peopleDetail: res.data[0]
-        })
-      });
-    db.collection('character_img').where({
-        cn: cn
-      }).limit(1)
-      .get().then(res => {
-        console.log(res);
-        let imgList = new Array();
-        if (res.data[0].stage0) {
-          imgList.push(res.data[0].stage0)
-        }
-        if (res.data[0].stage1) {
-          imgList.push(res.data[0].stage1)
-        }
-        if (res.data[0].stage2) {
-          imgList.push(res.data[0].stage2)
-        }
-        console.log(imgList);
-        this.setData({
-          peopleImageList: imgList,
-          name: cn
-        })
-      })
-    wx.cloud.callFunction({
-      // 云函数名称
-      name: 'getTalentList',
-      // 传给云函数的参数
-      data: {
-        name: this.data.name
-      }
-    }).then(res => {
-      this.setData({
-        talentList: res.result.talentList
-      })
-      console.log(res);
-    })
-    console.log(this.data.name);
-    wx.cloud.callFunction({
-      // 云函数名称
-      name: 'getBase',
-      // 传给云函数的参数
-      data: {
-        name: this.data.name
-      }
-    }).then(res => {
-      this.setData({
-        skillList: res.result.skillList
-      })
-      console.log(res);
-    })
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
+      let cn = this.data.name;
+      const db = wx.cloud.database({
+          env: 'test1-s9ptf'
+      });
+      db.collection('character')
+          .where({
+              name: cn
+          })
+          .limit(1)
+          .get().then(res => {
+              console.log(res);
+              this.setData({
+                  desc: res.data[0].itemDesc,
+                  peopleDetail: res.data[0]
+              })
+          });
+      db.collection('character_img').where({
+          cn: cn
+      }).limit(1)
+          .get().then(res => {
+              console.log(res);
+              let imgList = new Array();
+              if (res.data[0].stage0) {
+                  imgList.push(res.data[0].stage0)
+              }
+              if (res.data[0].stage1) {
+                  imgList.push(res.data[0].stage1)
+              }
+              if (res.data[0].stage2) {
+                  imgList.push(res.data[0].stage2)
+              }
+              console.log(imgList);
+              this.setData({
+                  peopleImageList: imgList,
+                  name: cn
+              })
+          })
+      wx.cloud.callFunction({
+          // 云函数名称
+          name: 'getTalentList',
+          // 传给云函数的参数
+          data: {
+              name: this.data.name
+          }
+      }).then(res => {
+          this.setData({
+              talentList: res.result.talentList
+          })
+          console.log(res);
+      })
+      console.log(this.data.name);
+      wx.cloud.callFunction({
+          // 云函数名称
+          name: 'getBase',
+          // 传给云函数的参数
+          data: {
+              name: this.data.name
+          }
+      }).then(res => {
 
+          this.setData({
+              skillList: res.result.skillList,
+              baseInfoList: res.result.baseInfoList
+          })
+          console.log(this.data.skillList);
+      })
   },
 
   /**
@@ -163,15 +168,15 @@ Page({
 
   },
   touchBaseInfo:function(){
-    if (this.data.talentState) {
+    if (this.data.baseInfoState) {
       this.setData({
-        talentState: !this.data.talentState,
-        talentButton: '展开'
+          baseInfoState: !this.data.baseInfoState,
+          baseInfoButton: '展开'
       })
     } else {
       this.setData({
-        talentState: !this.data.talentState,
-        talentButton: '收起'
+          baseInfoState: !this.data.baseInfoState,
+        baseInfoButton: '收起'
       })
     }
   }
