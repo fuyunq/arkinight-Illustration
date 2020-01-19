995f9b0abedb2d3b4200b9bcefcd12d066e5f6ee
fuyunqiang <fuyunq>
2020-01-19 16:35:46 +0800

    'linshi'

--- cloudfunctions/getBase/index.js
+++ cloudfunctions/getBase/index.js
@@ -20,74 +20,23 @@ exports.main = async(event, context) => {
   let skillList = [];
   let skills = res.data[0].skills;
   skillList = await getSkillList(skills);
-  // for (let i = 0; i < skills.length; i++) {
-  //   let id = skills[i].skillId;
-  //   let skilldata = await db.collection('skill_info')
-  //     .where({
-  //       skillId: id
-  //     }).limit(1)
-  //     .get();
-
-  //   let skillInfo = [];
-  //   let levels = skilldata.data[0].levels;
-  //   for (let j = 0; j < levels.length; j++) {
-  //     let skillLevelInfo ={};
-  //     let level = levels[j];
-  //     skillLevelInfo.name = level.name;
-  //     if (level.skillType == 1) {
-  //       skillLevelInfo.skillType = '自动触发';
-  //     } else if (level.skillType == 2) {
-  //       skillLevelInfo.skillType = '手动触发';
-  //     } else {
-
-  //     }
-  //     let spData = level.spData;
-  //     skillLevelInfo.initSp = spData.initSp;
-  //     skillLevelInfo.maxChargeTime = spData.maxChargeTime;
-  //     skillLevelInfo.spCost = spData.spCost;
-  //     if (spData.spType == 1) {
-  //       skillLevelInfo.spType = '自动回复';
-  //     } else if (spData.spType == 2) {
-  //       skillLevelInfo.spType = '攻击回复';
-  //     } else if (spData.spType == 2) {
-  //       skillLevelInfo.spType = '受击回复';
-  //     }
-  //     let blackBoard = level.blackboard;
-  //     let map = new Map();
-  //     let description = level.description;
-  //     let list = description.match(/<@ba.vup>.*?<\/>/gi);
-  //     for (let f = 0; f < list.length; f++) {
-  //       let str = list[f];
-        
-  //       for (let k = 0; k < blackBoard.length; k++) {
-  //         if (str.indexOf(blackBoard[k].key) != 1) {
-            
-  //           if (str.indexOf('%') != -1) {
-  //             let num = blackBoard[k].value * 100 + '%';
-  //             description = description.replace(str, num);
-              
-  //             break;
-  //           } else {
-  //             let num = blackBoard[k].value * 1;
-  //             description = description.replace(str, num);
-  //             break;
-              
-  //           }
-  //         }
-          
-  //       }
-  //     }
-  //     skillLevelInfo.desc = description;
-  //     skillInfo.push(skillLevelInfo);
-  //   }
-  //   skillList.push(skillInfo);
-  // }
+  
   return {
     skillList,
-    res
+    
   };
 }
-
+async function getBaseInfo(character){
+  let baseInfoList =[];
+  let phases = character.phases;
+  for(let i=0;i<phases;i++){
+    let phase = phases[i];
+    for (let k = 0; k < phase[k];k++){
+      let baseInfo ={};
+      
+    }
+  }
+}
 async function getSkillList(skills){
   let skillList = [];
   for (let i = 0; i < skills.length; i++) {
